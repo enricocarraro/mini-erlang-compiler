53cfaddfcb3a37a2fcb0c82b0200a49741714379
Enrico Carraro <enrico.carraro@studenti.polito.it>
2021-01-09 08:05:02 +0100

    42.

--- subex.erl
+++ subex.erl
@@ -1,12 +1,12 @@
 start() ->
-    Num = 43,
+    Num = 42,
     Pi = 3.14159,
     Hello = hello,
     OtherNode = example@node,
     StrangeString = "example@n\"ode",
     Listing = [1, 2, 3, 4].
 start(2) ->
-    Num = 43,
+    Num = 42,
     Pi = 3.14159,
     Hello = hello,
     OtherNode = example@node,
